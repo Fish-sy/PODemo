0d42497f793fe7c92365071ea44a47308729857f
Fish-sy <syy971128@163.com>
2019-12-23 17:23:50 +0800

    增加配置路径文件

---
+++ common/conf.py
@@ -0,0 +1,16 @@
+# coding = utf-8
+import os
+
+
+# 项目路径
+proPath = os.path.abspath(os.path.dirname(os.getcwd()))
+
+# 保存截图路径
+imagePath = os.path.join(proPath, "report", "Image")
+
+# 测试报告路径
+reportPath = os.path.join(proPath, "report", "TestReport")
+
+# 日志路径
+logPath = os.path.join(proPath, "report", "Log")
+
